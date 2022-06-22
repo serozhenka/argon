c5b9fa88b808c180f20b3f6d9f4da014dbd75c29
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-06-23 00:42:42 +0300

    added: post feed page

--- post/views.py
+++ post/views.py
@@ -21,29 +21,28 @@ def post_add_page(request):
         return render(request, 'post/post_add.html')
     elif request.method == "POST":
         images = request.FILES.getlist('images')
-        for i in range(50):
-            post = Post.objects.create(
-                user=request.user,
-                description=request.POST.get('description')
+        post = Post.objects.create(
+            user=request.user,
+            description=request.POST.get('description')
+        )
+
+        for i in range(0, len(images)):
+            post_img = PostImage.objects.create(
+                post=post,
+                image=images[i],
+                order=i,
             )
+            absolute_url = str(settings.BASE_DIR) + post_img.image.url
+            img = cv2.imread(absolute_url)
+            width = int(img.shape[1])
+            height = int(img.shape[0])
 
-            for i in range(0, len(images)):
-                post = PostImage.objects.create(
-                    post=post,
-                    image=images[i],
-                    order=i,
-                )
-                absolute_url = str(settings.BASE_DIR) + post.image.url
-                img = cv2.imread(absolute_url)
-                width = int(img.shape[1])
-                height = int(img.shape[0])
+            if height / width > 1.25:  # 5 / 4
+                img = img[int(height/2 - 5/8*width):int(height/2 + 5/8*width), 0:width]
+            elif height / width < 0.8:  # 4 / 5
+                img = img[0:height, int(width/2 - 5/8*height):int(width/2 + 5/8*height)]
 
-                if height / width > 1.25:  # 5 / 4
-                    img = img[int(height/2 - 5/8*width):int(height/2 + 5/8*width), 0:width]
-                elif height / width < 0.8:  # 4 / 5
-                    img = img[0:height, int(width/2 - 5/8*height):int(width/2 + 5/8*height)]
-
-                cv2.imwrite(absolute_url, img)
+            cv2.imwrite(absolute_url, img)
 
         return redirect('account:account', request.user.username)
 
