ec2671617b07435a4b556e85a93951a741fd3962
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-07-07 01:56:37 +0300

    added: updating post app notifications in real time

--- post/signals.py
+++ post/signals.py
@@ -1,10 +1,13 @@
 from django.conf import settings
 from django.contrib.contenttypes.fields import ContentType
-from django.db.models.signals import post_delete, post_save, pre_save
+from django.db.models.signals import post_delete, post_save, pre_delete
 from django.dispatch import receiver
 from django.urls import reverse
 
 from .models import PostImage, Comment, PostLike
+from .utils import clear_post_like_notifications
+from notifications.models import Notification
+from notifications.utils import send_notification_to_channel_layer, send_notification_delete_to_channel_layer
 
 @receiver(post_delete, sender=PostImage)
 def post_cascade_delete_image(sender, instance, *args, **kwargs):
@@ -16,7 +19,8 @@ def post_cascade_delete_image(sender, instance, *args, **kwargs):
 @receiver(post_save, sender=Comment)
 def create_post_comment_notification(sender: Comment, instance: Comment, created, **kwargs):
     if created and instance.user != instance.post.user:
-        instance.notifications.create(
+
+        notification = instance.notifications.create(
             sender=instance.user,
             receiver=instance.post.user,
             action_name="post_comment",
@@ -25,10 +29,29 @@ def create_post_comment_notification(sender: Comment, instance: Comment, created
             content_type=ContentType.objects.get_for_model(instance),
         )
 
+        send_notification_to_channel_layer(notification, [])
+
+@receiver(pre_delete, sender=Comment)
+def delete_post_comment_send_delete_notification(sender: Comment, instance: Comment, **kwargs):
+    if instance.user != instance.post.user:
+
+        notification = Notification.objects.get(
+            sender=instance.user,
+            receiver=instance.post.user,
+            action_name="post_comment",
+            object_id=instance.id,
+        )
+        send_notification_delete_to_channel_layer(instance.post.user.username, [notification.id])
+
 @receiver(post_save, sender=PostLike)
 def create_post_like_notification(sender: PostLike, instance: PostLike, created, **kwargs):
-    if instance.is_liked and instance.user != instance.post.user:
-        instance.notifications.create(
+    if instance.user == instance.post.user:
+        return
+
+    notifications_to_delete_id_list = clear_post_like_notifications(instance, instance.user, instance.post.user)
+
+    if instance.is_liked:
+        notification = instance.notifications.create(
             sender=instance.user,
             receiver=instance.post.user,
             action_name="post_like",
@@ -36,4 +59,11 @@ def create_post_like_notification(sender: PostLike, instance: PostLike, created,
             redirect_url=f"{settings.BASE_URL}{reverse('post:post-page', kwargs={'post_id': instance.post.id})}",
             content_type=ContentType.objects.get_for_model(instance),
         )
+
+        send_notification_to_channel_layer(notification, notifications_to_delete_id_list)
+
+    elif not instance.is_liked:
+        send_notification_delete_to_channel_layer(instance.post.user.username, notifications_to_delete_id_list)
+
+
 
--- post/utils.py
+++ post/utils.py
@@ -1,4 +1,8 @@
-from .models import PostLike
+from django.conf import settings
+from django.contrib.contenttypes.fields import ContentType
+
+from .models import PostLike, Post
+from notifications.models import Notification
 
 def is_post_liked_by_user(user, post):
     try:
@@ -17,3 +21,17 @@ def private_account_action_permission(user, post_user):
     ):
         return False
     return True
+
+def clear_post_like_notifications(post_like: PostLike, sender: settings.AUTH_USER_MODEL, receiver: settings.AUTH_USER_MODEL) -> list:
+    notifications_to_delete = Notification.objects.filter(
+        sender=sender,
+        receiver=receiver,
+        action_name="post_like",
+        content_type=ContentType.objects.get_for_model(post_like),
+        object_id=post_like.id,
+    )
+    notifications_to_delete_id_list = list(notifications_to_delete.values_list('id', flat=True))
+    notifications_to_delete.delete()
+    print(notifications_to_delete_id_list)
+
+    return notifications_to_delete_id_list
